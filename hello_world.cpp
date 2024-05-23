61f1a70c3a3ab20d45508ee4cc83b04b200da602
naibyte <128290084+naibyte@users.noreply.github.com>
2024-05-22 16:27:00 -1000

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,16 +1,9 @@
 #include <HardwareSerial.h>
-extern "C" void __cxa_pure_virtual() { while(1);};
 
 void setup() {
+	Serial.begin(115200);
 };
 
 void loop() {
 	Serial.println("Hello World");
-	loop();
-};
-
-int main() {
-	Serial.begin(115200);
-	loop();
-	return 0;
 };
